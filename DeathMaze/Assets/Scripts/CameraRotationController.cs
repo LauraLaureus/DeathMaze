4dd14454d7db860fbdbe95899e06d8ec2bba6986
Laura <l4depende@gmail.com>
2016-11-06 20:36:53 +0100

    Camera Rotation

---
+++ DeathMaze/Assets/Scripts/CameraRotationController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotationController : MonoBehaviour {
+
+	public float step = 1.0f;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Rotate(Time.deltaTime,-step,0);
+		}
+
+		if(Input.GetKey(KeyCode.RightArrow)){
+			transform.Rotate(Time.deltaTime,step,0);
+		}
+			
+	}
+}
