39e4eb902d68733b0965587e3777f485b56d215c
Laura <l4depende@gmail.com>
2016-11-08 21:58:16 +0100

    Portal asset created and programmed

---
+++ DeathMaze/Assets/PortalController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PortalController : MonoBehaviour {
+
+	void OnCollisionEnter(Collision other){
+		SceneManager.LoadScene ("Main");
+	}
+}
